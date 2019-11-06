3f2008efe43d2f5099fea717e9d680bf22371f37
junnys6018 <48525532+junnys6018@users.noreply.github.com>
2019-11-06 23:07:32 +0800

    Ch 12

--- OneWeekend/src/Camera.h
+++ OneWeekend/src/Camera.h
@@ -5,11 +5,16 @@
 class Camera
 {
 public:
-	Camera(glm::vec3 lookFrom, glm::vec3 lookAt, glm::vec3 up, float vfov, float aspect); // vfov is top to bottom in radians
+	Camera(glm::vec3 lookFrom, glm::vec3 lookAt, glm::vec3 up, float vfov, float aspect, float apature, float focusDist); // vfov is top to bottom in radians
 	Ray getRay(float u, float v) const;
 private:
 	glm::vec3 m_origin;
 	glm::vec3 m_lowerLeft;
 	glm::vec3 m_horizontal;
 	glm::vec3 m_vertical;
+	glm::vec3 m_u, m_v, m_w;
+	float m_lensRadius;
+
+private:
+	glm::vec3 randomInUnitDisk() const;
 };
